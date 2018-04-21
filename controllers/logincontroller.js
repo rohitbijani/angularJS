6da6c74d1f7c8efe6647dbace3c4fb15b38e7965
rohitbijani <rohit.bijani007@gmail.com>
2018-04-21 18:23:07 +0530

    login updated

---
+++ controllers/logincontroller.js
@@ -0,0 +1,14 @@
+/****
+*@author  
+*@date 
+*@purpose 
+*@description 
+****/
+
+/**
+*@method : fetch
+*@params : { studentName : string,rollNumber:number}
+*@return : {} : Student 
+**/
+function fetch(studentName,rollNumber){
+}
