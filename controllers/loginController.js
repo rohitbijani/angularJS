cbbac0d06ac191abd8b93cd636def9618ebe0be2
rohitbijani <rohit.bijani007@gmail.com>
2018-04-25 19:24:51 +0530

    25/4/18

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,18 @@
 *@purpose
 *@description
 ****/
- myApp.controller('loginController', function($scope){
-   $scope.uname="";
-   $scope.password="";
+ myApp.controller('loginController', function($scope,$state){
+   $scope.word = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   $scope.validate = function() {
+     $scope.display="";
+     var x = document.forms["form"]["email"].value;
+     var y = document.forms["form"]["password"].value;
+
+     if(x!="bridgeit@gmail.com" || y!="bridgeit"){
+       $scope.display="Incorrect EmailID or Password!"
+     }
+     else {
+       $state.go('home');
+     }
+   }
  });
