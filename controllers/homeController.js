8e4e7e5d8abb876e0266dedf00c9d670705a7ecb
rohitbijani <rohit.bijani007@gmail.com>
2018-04-30 19:25:54 +0530

    30/4/18

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,6 @@
 myApp.controller('homeController',function($scope,$mdSidenav){
   $scope.toggleLeft=buildToggler('left');
-  $scope.isOpenLeft = function(){
-    return $mdSidenav('left').isOpen();
-  };
-
+    
   function buildToggler(componentID) {
       return function() {
         $mdSidenav(componentID).toggle();
--- services/jsonRead.js
+++ services/jsonRead.js
@@ -2,7 +2,8 @@ myApp.service('jsonRead', function() {
     this.read = function (x) {
         return {
           function ($scope,$http) {
-            $http.get("assets/products.json").success(function(response) {$scope.names = response;});
+            $http.get("assets/products.json").then(function(response) {$scope.names = response;});
+            console.log($scope.names);
           }
         };
       }
